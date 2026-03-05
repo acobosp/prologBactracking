5691183bd0fccfd26d5405f3ddb0a83dd72b3f09
Albert Cobos <albert@gmail.com>
2026-03-05 12:50:57 -0500

    add primeros hechos

---
+++ ejercicio.pl
@@ -0,0 +1,8 @@
+conexion(vancouver,edmonton,16).
+conexion(vancouver,calgary,13).
+conexion(edmonton,saskatoon,12).
+conexion(saskatoon,calgary,9).
+conexion(saskatoon,winninpeg,20).
+conexion(calgary,regina,14).
+conexion(regina,saskatoon,7).
+conexion(regina,winninpeg,4).
